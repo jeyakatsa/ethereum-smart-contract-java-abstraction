676de4be86b29502c6517a81bfbdcae0321f5bbe
Jeremy Akatsa <jeyakatsa@gmail.com>
2022-01-12 18:57:51 -0800

    working on new function

--- Smart-Contract-Reference/src/main/java/Subcurrency.java
+++ Smart-Contract-Reference/src/main/java/Subcurrency.java
@@ -2,9 +2,15 @@ import java.util.*;
 
 public class Subcurrency {
 
-    //Address & Uint256 dependencies to be imported
+    //Address, Uint256 & Event dependencies to be imported
     public Address minter;
-    public Map balances = new Map<Adress, Uint256>();
+    private Map balances = new Map<Adress, Uint256>();
+
+    // Events allow clients to react to specific
+    // contract changes you declare
+    private Event sent(Address from, Address to, Uint256 ammount) {
+        return null;
+    }
 
 
 }
