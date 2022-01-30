41441c81d336dc5cb2f29f7c52fbda38105a0c10
Jeremy Akatsa <jeyakatsa@gmail.com>
2022-01-29 17:29:17 -0800

    Base LinkedList within Msg depencency built

--- smart-contract-dependencies/src/main/java/Msg.java
+++ smart-contract-dependencies/src/main/java/Msg.java
@@ -1,4 +1,18 @@
+import java.util.*;
+
 public class Msg {
+
+    Address head;
+
+    class Address {
+        byte[] data;
+        Address sender;
+        Address next;
+
+        Address(byte[] d) {
+            data = d;
+        }
+    }
 
     //Code to be added
 
