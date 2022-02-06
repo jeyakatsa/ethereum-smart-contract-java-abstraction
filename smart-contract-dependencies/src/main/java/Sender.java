cc780e4661e66a1ac01dab2cbd3a66dc7b608172
Jeremy Akatsa <jeyakatsa@gmail.com>
2022-02-05 17:27:37 -0800

    Data dependency completed

--- smart-contract-dependencies/src/main/java/Sender.java
+++ smart-contract-dependencies/src/main/java/Sender.java
@@ -2,11 +2,12 @@ public interface Sender extends Address {
     Sender head = (Sender) new Address();
 
     public static class Address {
-        byte[] data;
+        //Byte[] function needs to call Data dependency in place (To Be Refactored)
+        Data data;
         Sender sender;
         Sender next;
 
-        void Sender(byte[] d) {
+        void Sender(Data d) {
             data = d;
         }
 
