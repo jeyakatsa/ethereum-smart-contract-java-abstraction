5aaa1a0d1f23474d66b3ba19edddc07271e0715b
Jeremy Akatsa <jeyakatsa@gmail.com>
2022-02-16 23:36:36 -0800

    Compiler output skeleton

--- smart-contract-compilation-scripts/compile-outputDir.java
+++ smart-contract-compilation-scripts/compile-outputDir.java
@@ -1,1 +1,9 @@
-//Code to be added
+
+static final datatype path = require('path');
+static final datatype fs = require('fs-extra');
+
+static final datatype buildPath = path.resolve('ethereum', 'build');
+
+static final datatype createBuildFolder = () => {
+    fs.emptyDirSync(buildpath);
+        }
